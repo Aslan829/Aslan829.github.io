d2253e8c2967f9d0f12ba689813db65798e35f9d
aslan829 <ashleyoffret1@gmail.com>
2017-07-01 16:05:46 -0600

    update

--- Weather/search-java.js
+++ Weather/search-java.js
@@ -2,25 +2,25 @@ var returned;
 
 function getData(input) {
 	$.ajax({
-		url: "//api.wunderground.com/api/fc63cfd847a03075/geolookup/conditions/q/" + input + ".json",
+		url: "//api.wunderground.com/api/fc63cfd847a03075/geolookup/conditions/forecast/q/" + input + ".json",
 		dataType: "jsonp",
 		success: function (data) {
 			console.log(data);
 			var location = data.location.city + ', ' + data.location.state;
 			var temp_f = data.current_observation.temp_f;
-			var Wspeed = data.current_observation.wind_mph;
-			var feelLike= data.current_observation.feelslike_f;
+			var tempHigh = data.forecast.simpleforecast.forecastday.high.;
+			//var tempLow= data.simpleforecast.forecastday.low;
 			console.log('Location:' + location);
-			console.log('temp: ' + temp_f);
+			console.log('temp: ' +temp_f);
 			//Will add connection soon, just need to see if it works
 			$("#overview").text(location);
 			$("title").html(location + " | Weather Center");
 			$("#cityDisplay").html(location);
 			$("#currentTemp").html(Math.round(temp_f) + '°F');
-			$("#summary").html(toTitleCase(data.current_observation.icon));
+			$("#summary").html(toTitleCase(data.current_observation.weather));
 			$("#status").text("");
-			$("#add1").html("The wind speed today is "+Wspeed+"MPH");
-			$("#add2").html("It actually feels like "+feelLike+"°F");
+			$("#add1").html("THIS IS"+tempHigh);
+			//$("#add2").html("It actually feels like "+feelLike+"°F");
 
 		}
 	});
